c1f699c8bd7e6ceeece84f5253e0520bb4776cd9
rh314 <a@b.c>
2019-04-21 20:08:06 +0200

    Set environment and create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+set -e
+
+docker build \
+    -t rhtakealot/multi-client:latest \
+    -t rhtakealot/multi-client:$SHA \
+    -f ./client/Dockerfile ./client
+docker build \
+    -t rhtakealot/multi-server:latest \
+    -t rhtakealot/multi-server:$SHA \
+    -f ./server/Dockerfile ./server
+docker build \
+    -t rhtakealot/multi-worker:latest \
+    -t rhtakealot/multi-worker:$SHA \
+    -f ./worker/Dockerfile ./worker
+
+# client
+docker push rhtakealot/multi-client:latest
+docker push rhtakealot/multi-client:$SHA
+# server
+docker push rhtakealot/multi-server:latest
+docker push rhtakealot/multi-server:$SHA
+# worker
+docker push rhtakealot/multi-worker:latest
+docker push rhtakealot/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment \
+    server=rhtakealot/multi-server:$SHA
+kubectl set image deployments/client-deployment \
+    client=rhtakealot/multi-client:$SHA
+kubectl set image deployments/worker-deployment \
+    worker=rhtakealot/multi-worker:$SHA
